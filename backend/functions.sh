eddf55d2e44168c26f3217269a575bffc83e82b6
pkgdemon <pkgdemonteam@gmail.com>
2017-09-29 13:03:48 -0400

    Also keep HOME variable for sudo * This fixes ssh key generation for user when running sudo.

--- backend/functions.sh
+++ backend/functions.sh
@@ -82,7 +82,7 @@ install_dependencies()
   if [ ! -f "/usr/local/etc/sudoers.d/ixautomation" ] ; then
     touch /usr/local/etc/sudoers.d/ixautomation
     echo '%jenkins ALL = NOPASSWD: /ixautomation/jenkins.sh' >> /usr/local/etc/sudoers.d/ixautomation
-    echo 'Defaults      env_keep += "SSH_AUTH_SOCK"' >> /usr/local/etc/sudoers.d/ixautomation
+    echo 'Defaults      env_keep += "SSH_AUTH_SOCK HOME"' >> /usr/local/etc/sudoers.d/ixautomation
   fi
 
 }
