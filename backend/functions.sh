f36e07a7343fa926ea1999a78098a498bd0dc537
ericbsd <ericturgeon.bsd@gmail.com>
2017-10-03 20:29:39 -0300

    Change the grep for Linux instead of Ubuntu

--- backend/functions.sh
+++ backend/functions.sh
@@ -89,7 +89,7 @@ install_dependencies()
 
 install_dependencies_webui()
 {
-  uname -a | grep "Ubuntu" >/dev/null
+  uname -a | grep "Linux" >/dev/null
   if [ $? -eq 0 ] ; then
     cd ~/
     apt-get install openssh-server
