4ffb388cd294effe5aea48e382037b008fe84260
ericbsd <ericturgeon.bsd@gmail.com>
2017-10-18 16:22:35 -0300

    Fix --password for the right password of the created bhyve

--- backend/functions.sh
+++ backend/functions.sh
@@ -214,7 +214,7 @@ jenkins_freenas_api_tests()
   bhyve_install_iso
   bhyve_boot
   cd "${MASTERWRKDIR}/freenas/api-test" || exit_clean
-  python runtest.py --ip ${FNASTESTIP} --password abcd1234 --interface vtnet0
+  python runtest.py --ip ${FNASTESTIP} --password testing --interface vtnet0
   cd -
 }
 
