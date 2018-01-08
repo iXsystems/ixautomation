1cb3eb0f0b76e1b4739f9a6fd7bf88020fbe993e
ericbsd <ericturgeon.bsd@gmail.com>
2018-01-08 16:27:54 -0400

    Adding BSD TEST code to iscsi.py

--- tests/freenas/api-test/create/iscsi.py
+++ tests/freenas/api-test/create/iscsi.py
@@ -9,7 +9,8 @@ import sys
 import os
 apifolder = os.getcwd()
 sys.path.append(apifolder)
-from functions import PUT, POST, GET_OUTPUT
+from functions import PUT, POST, GET_OUTPUT, BSD_TEST
+from auto_config import ip
 
 try:
     from config import BRIDGEHOST
@@ -26,7 +27,9 @@ class iscsi_test(unittest.TestCase):
     def test_01_Clean_up_any_leftover_items(self):
         payload = {"srv_enable": "false"}
         assert PUT("/services/services/iscsitarget/", payload) == 200
-        # When BSD_TEST is functional will need to add the code to clean test
+        BSD_TEST("iscsictl -R -t TARGET_NAME" % TARGET_NAME)
+        BSD_TEST('umount -f "%s" &>/dev/null' % MOUNTPOINT)
+        BSD_TEST('rmdir "%s" &>/dev/null' % MOUNTPOINT)
 
     # Add iSCSI initator
     def Test_02_Add_iSCSI_initiator(self):
@@ -83,13 +86,73 @@ class iscsi_test(unittest.TestCase):
                           "srv_state") == "RUNNING"
 
     # when BSD_TEST is functional test using it will need to be added
+    # Now connect to iSCSI target
+    def test_10_Connecting_to_iSCSI_target(self):
+        cmd = 'iscsictl -A -p %s:3620 -t %s' % (ip, TARGET_NAME)
+        assert BSD_TEST(cmd) is True
+
+    def test_11_Waiting_for_iscsi_connection_before_grabbing_device_name(self):
+        # local loop_cnt = 0
+        # while [ $loop_cnt -le 12 ] ; do
+        assert BSD_TEST("iscsictl -L") is True
+        #    iscsi_state=$(cat /tmp/.bsdCmdTestStdOut | awk '$2 == "'${BRIDGEIP}':3620" {print $3}')
+        #    iscsi_dev=$(cat /tmp/.bsdCmdTestStdOut | awk '$2 == "'${BRIDGEIP}':3620" {print $4}')
+
+        # if [ -n "${iscsi_state}" -a "${iscsi_state}" == "Connected:" ]:
+        #    if [ -n "${iscsi_dev}" ] ; then
+        #        DEVICE_NAME=$iscsi_dev
+        #        echo -n " using \"${DEVICE_NAME}\""
+        #        echo_ok && break
+        #    else:
+        #        echo -n "... connected with no device"
+        #    loop_cnt=$(expr $loop_cnt + 1)
+        #    [ $loop_cnt -gt 12 ] && echo_fail && return 1
+        #    echo -n "."
+        #    sleep 3
+        # done
+
+    def test_12_Format_the_target_volume(self):
+        assert BSD_TEST('newfs "/dev/%s"' % DEVICE_NAME) is True
+
+    def test_13_Creating_iSCSI_mountpoint(self):
+        assert BSD_TEST('mkdir -p "%s"' % MOUNTPOINT) is True
+
+    def test_14_Mount_the_target_volume(self):
+        cmd = 'mount "/dev/%s" "%s"' % (DEVICE_NAME, MOUNTPOINT)
+        assert BSD_TEST(cmd) is True
+
+    def test_15_Creating_file(self):
+        cmd = 'touch "%s/testfile"' % MOUNTPOINT
+        # The line under doesn't make sence
+        # "umount '${MOUNTPOINT}'; rmdir '${MOUNTPOINT}'"
+        assert BSD_TEST(cmd) is True
+
+    def test_16_Moving_file(self):
+        cmd = 'mv "%s/testfile" "%s/testfile2"' % (MOUNTPOINT, MOUNTPOINT)
+        assert BSD_TEST(cmd) is True
+
+    def test_17_Copying_file(self):
+        cmd = 'cp "%s/testfile2" "%s/testfile"' % (MOUNTPOINT, MOUNTPOINT)
+        assert BSD_TEST(cmd) is True
+
+    def test_18_Deleting_file(self):
+        assert BSD_TEST('rm "%s/testfile2"' % MOUNTPOINT) is True
+
+    def test_19_Unmounting_iSCSI_volume(self):
+        assert BSD_TEST('umount "%s"' % MOUNTPOINT) is True
+
+    def test_20_Removing_iSCSI_volume_mountpoint(self):
+        assert BSD_TEST('rmdir "%s"' % MOUNTPOINT) is True
+
+    def test_21_Disconnect_all_targets(self):
+        assert BSD_TEST('iscsictl -R -t %s' % TARGET_NAME) is True
 
     # Disable the iSCSI service
-    def test_10_Disable_iSCSI_service(self):
+    def test_22_Disable_iSCSI_service(self):
         payload = {"srv_enable": "false"}
         assert PUT("/services/services/iscsitarget/", payload) == 200
 
-    def test_11_Verify_the_iSCSI_service_is_disabled(self):
+    def test_23_Verify_the_iSCSI_service_is_disabled(self):
         assert GET_OUTPUT("/services/services/iscsitarget/",
                           "srv_state") == "STOPPED"
 
