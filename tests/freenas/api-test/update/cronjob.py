2b1cad87c074aa051f75e88bf7386a7c2a1a25fa
ericbsd <ericturgeon.bsd@gmail.com>
2018-01-10 16:08:46 -0400

    Add SSH test to update cronjob.py

--- tests/freenas/api-test/update/cronjob.py
+++ tests/freenas/api-test/update/cronjob.py
@@ -9,7 +9,7 @@ import sys
 import os
 apifolder = os.getcwd()
 sys.path.append(apifolder)
-from functions import PUT, GET_OUTPUT
+from functions import PUT, GET_OUTPUT, SSH_TEST
 
 TESTFILE = "/tmp/.testFileCreatedViaCronjob"
 CRONJOB_ID = 1
@@ -18,8 +18,8 @@ CRONJOB_ID = 1
 class cronjob_test(unittest.TestCase):
 
     # Ensure test file does exist
-    # def test_01_Verify_cronjob_has_created_the_test_file(seff)
-    #     assert SSH_TEST("test -f " + TESTFILE) == True
+    def test_01_Verify_cronjob_has_created_the_test_file(seff):
+        assert SSH_TEST("test -f " + TESTFILE) == True
 
     # Update cronjob to disabled with new cron_command
     def test_02_Updating_cron_job_status_to_disabled_updating_command(self):
@@ -33,8 +33,8 @@ class cronjob_test(unittest.TestCase):
 
     # Delete test file so we can verify it is no longer being created later
     # in the delete/cronjob test
-    # def test_02_Deleting_test_file_created_by_cronjob(self):
-    #     SSH_TEST("rm -f " + TESTFILE) is True
+    def test_02_Deleting_test_file_created_by_cronjob(self):
+        SSH_TEST("rm -f " + TESTFILE) is True
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
