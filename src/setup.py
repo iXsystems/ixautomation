0470a22b330b93a89355106846a95c2cdc04d43a
ericbsd <ericturgeon.bsd@gmail.com>
2021-12-21 16:00:31 -0400

    Added container specific code to install setup.py
    related to #214

--- src/setup.py
+++ src/setup.py
@@ -3,12 +3,16 @@
 # Author: Eric Turgeon
 # License: BSD
 
-import sys
+import os
+import platform
+import shutil
 from setuptools import setup
 
-__VERSION__ = '21.12.10'
+__VERSION__ = '21.12.21'
 
 PROGRAM_VERSION = __VERSION__
+
+system = platform.system()
 
 # Hardcode prefix to /usr/local for BSD, Debian and Docker
 prefix = '/usr/local'
@@ -22,6 +26,9 @@ etc_list = [
     'etc/ixautomation.conf.dist',
     'etc/dnsmasq.conf'
 ]
+
+if system == 'FreeBSD':
+    etc_list.append('etc/smb4.conf')
 
 dot_templates_list = [
     'ixautomation/vms/.templates/freenas.conf',
@@ -42,6 +49,10 @@ data_files = [
     (f'{prefix}/lib/ixautomation', lib_ixautomation_list)
 ]
 
+if system == 'Linux':
+    shutil.copyfile('etc/smb4.conf', 'etc/smb.conf')
+    data_files.append(('/etc/samba', ['etc/smb.conf']))
+
 setup(
     name="ixautomation",
     version=PROGRAM_VERSION,
@@ -54,3 +65,7 @@ setup(
     install_requires=['setuptools'],
     scripts=['bin/ixautomation']
 )
+
+
+if system == 'Linux':
+    os.remove('etc/smb.conf')
