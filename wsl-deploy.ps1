d24eea5ddcc1353b7e3a5f6d2c9d7a9040f26218
Joe Maloney <pkgdemonteam@gmail.com>
2021-12-22 18:10:16 -0500

    Update wsl-deploy.ps1

--- wsl-deploy.ps1
+++ wsl-deploy.ps1
@@ -2,7 +2,6 @@ docker build -t ixautomation -f Dockerfile .
 docker export -o ixautomation.tar $(docker run -d ixautomation /bin/true)
 wsl --import ixautomation c:\ixautomation ixautomation.tar --version 2
 $WSLUSER = Read-Host "Please enter your username"
-wsl -d ixautomation useradd $WSLUSER
-wsl -d ixautomation passwd $WSLUSER
+wsl -d ixautomation adduser $WSLUSER
 wsl -d ixautomation usermod -aG sudo $WSLUSER
 wsl -d ixautomation echo -e "[user]\ndefault=$WSLUSER" >> /etc/wsl.conf
