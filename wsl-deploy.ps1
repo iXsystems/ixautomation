4e25529967bf7a79ed0d3f06c2b3c1d7508276d0
Joe Maloney <pkgdemonteam@gmail.com>
2021-12-22 18:23:40 -0500

    Update wsl-deploy.ps1

--- wsl-deploy.ps1
+++ wsl-deploy.ps1
@@ -4,5 +4,3 @@ wsl --import ixautomation c:\ixautomation ixautomation.tar --version 2
 $WSLUSER = Read-Host "Please enter your username"
 wsl -d ixautomation adduser $WSLUSER
 wsl -d ixautomation usermod -aG sudo $WSLUSER
-wsl -d ixautomation echo -e "[user]\ndefault=$WSLUSER" >> wsl.conf
-wsl -d ixautomation mv wsl.conf /etc/wsl.conf
