7f7dba1a7eeb31314bbe241fe7fea4f6acccf464
Joe Maloney <pkgdemonteam@gmail.com>
2021-12-22 17:34:39 -0500

    Update wsl-deploy.bat

--- wsl-deploy.bat
+++ wsl-deploy.bat
@@ -1,4 +1,3 @@
-curl https://raw.githubusercontent.com/iXsystems/ixautomation/master/Dockerfile -o Dockerfile
 docker build -t ixautomation -f Dockerfile .
 docker export -o ixautomation.tar $(docker run -d ixautomation /bin/true)
 wsl --import ixautomation c:\ixautomation ixautomation.tar --version 2
