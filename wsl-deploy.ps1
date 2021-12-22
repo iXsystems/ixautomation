797da93bf33fe4bd34f0bd09e003c55f4948b1f8
Joe Maloney <pkgdemonteam@gmail.com>
2021-12-22 18:00:44 -0500

    Ask for user and add default user

--- wsl-deploy.ps1
+++ wsl-deploy.ps1
@@ -1,3 +1,7 @@
 docker build -t ixautomation -f Dockerfile .
 docker export -o ixautomation.tar $(docker run -d ixautomation /bin/true)
 wsl --import ixautomation c:\ixautomation ixautomation.tar --version 2
+wsl -d ixautomation useradd $WSLUSER
+wsl -d ixautomation passwd $WSLUSER
+wsl -d ixautomation usermod -aG sudo $WSLUSER
+wsl -d ixautomation echo -e "[user]\ndefault=$WSLUSER" >> /etc/wsl.conf
