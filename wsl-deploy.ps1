a3de538d13211076676cd5799dad64b966344662
Joe Maloney <pkgdemonteam@gmail.com>
2021-12-22 20:42:47 -0500

    Remove code to create user
    * To be done later with proper launcher the official way.

--- wsl-deploy.ps1
+++ wsl-deploy.ps1
@@ -1,6 +1,3 @@
 docker build -t ixautomation -f Dockerfile .
 docker export -o ixautomation.tar $(docker run -d ixautomation /bin/true)
 wsl --import ixautomation c:\ixautomation ixautomation.tar --version 2
-$WSLUSER = Read-Host "Please enter your username"
-wsl -d ixautomation adduser $WSLUSER
-wsl -d ixautomation usermod -aG sudo $WSLUSER
