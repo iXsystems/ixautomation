5d0fcb7d665bc1618c6b769f7e4efda3e1dd71d1
ericbsd <ericturgeon.bsd@gmail.com>
2022-01-19 12:44:06 -0400

    Adding new profile for TrueNAS 13.0

--- src/setup.py
+++ src/setup.py
@@ -41,7 +41,8 @@ if system == 'FreeBSD':
 
 dot_templates_list = [
     'ixautomation/vms/.templates/freenas.conf',
-    'ixautomation/vms/.templates/freenas_webui.conf'
+    'ixautomation/vms/.templates/freenas_webui.conf',
+    'ixautomation/vms/.templates/truenas13.conf'
 ]
 
 lib_ixautomation_list = [
