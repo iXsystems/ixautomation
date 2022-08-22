04a6b0661c45f1f7f675cf9c1850d6da7807ea33
Joe Maloney <jpm820@gmail.com>
2022-08-21 21:51:21 -0400

    Delete wsl-deploy.ps1

--- wsl-deploy.ps1
+++
@@ -1,3 +0,0 @@
-docker build -t ixautomation -f Dockerfile .
-docker export -o ixautomation.tar $(docker run -d ixautomation /bin/true)
-wsl --import ixautomation c:\ixautomation ixautomation.tar --version 2
