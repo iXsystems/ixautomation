79c91c275e7d832173d9bf8aaeb5872fb8b1e1f5
ericbsd <ericturgeon.bsd@gmail.com>
2023-11-17 18:40:05 -0400

    adding efi firmware

--- src/lib/ixautomation/functions_vm.py
+++ src/lib/ixautomation/functions_vm.py
@@ -169,9 +169,7 @@ def kvm_create_disks(vm_name, profile):
 
 
 def kvm_install_vm(vm_data_dir, vm_name, xml_template, iso_path, profile):
-    cdrom = 'sdn' if profile == 'kvm_scale' else 'sde'
-    print(cdrom)
-    print(profile)
+    cdrom = 'sde'
     run(f'virsh define {xml_template}', shell=True)
     sleep(1)
     run(f'virsh change-media {vm_name} {cdrom} {iso_path}', shell=True)
